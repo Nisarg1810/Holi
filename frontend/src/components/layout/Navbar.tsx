15181ba7bdeb087036c2624e77e05f41a026ce7d
Nisarg Patel <pateln1810@gmail.com>
2026-07-30 23:52:59 +0530

    style: removed redundant Home text link from navbar as clicking brand logo routes to homepage

--- frontend/src/components/layout/Navbar.tsx
+++ frontend/src/components/layout/Navbar.tsx
@@ -42,7 +42,6 @@ export default function Navbar() {
   }
 
   const navLinks = [
-    { name: "Home", href: "/" },
     { name: "Services", href: "/services" },
     { name: "Guides", href: "/blog" },
     { name: "Careers", href: "/careers" },
